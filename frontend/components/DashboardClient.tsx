be27deac3432ea15f54d8ef2889edc8e676d21fe
hyobin <hbaloha@naver.com>
2026-07-09 11:33:22 +0900

    Fix ESLint warnings

--- frontend/components/DashboardClient.tsx
+++ frontend/components/DashboardClient.tsx
@@ -37,8 +37,6 @@ export default function DashboardClient({
 
     useEffect(() => {
         if (!selectedSession) {
-            setSummary(null);
-            setTrajectory([]);
             return;
         }
 
